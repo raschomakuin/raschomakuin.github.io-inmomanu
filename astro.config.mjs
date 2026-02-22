c9690d8fd68bb3cf764bce830f05e6c711422d5c
raschomakuin <rodripalacio14@gmail.com>
2026-02-22 15:30:35 -0300

    Simplify astro.config.mjs by removing site and base
    Removed site and base properties from configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,1 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
-export default defineConfig({
-  site: "https://raschomakuin.github.io",
-  base: "/raschomakuin.github.io-inmomanu/",
-  integrations: [tailwind()],
-  output: 'static',
-  compressHTML: true,
-  build: {
-    inlineStylesheets: 'auto'
-  }
-});
+{ defineConfig } from 'astro/config'; import tailwind from '@astrojs/tailwind'; // https://astro.build/config export default defineConfig({ integrations: [tailwind()], output: 'static', compressHTML: true, build: { inlineStylesheets: 'auto' } });
